93f1074c3c29399bd9be2287c4c0e6278962c31b
Jason M. Rimer <Atlas@Jason-Michael-Rimers-MacBook-Air.local>
2012-12-12 19:54:18 -0500

    Initial Commit

---
+++ CMSCProject_2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CMSCProject_2
+//
+//  Created by Jason M. Rimer on 12/12/12.
+//  Copyright (c) 2012 Jason M. Rimer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
